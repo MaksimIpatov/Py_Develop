6c2e9faaed607d7030bf50ed3d74f37ddda14ab8
maksim ipatov <ipatovmaksim78@gmail.com>
2024-04-03 21:35:28 +0500

    Def_Function_File "Function "

--- str_func.py
+++ str_func.py
@@ -1,10 +1,10 @@
-def upper(x):
+def upper(x): """Функция написана"""
     for c in x:
         if c.isupper():
             return(c)
         elif x.islower():
             return('')
-def Capitalize(S):    'function создана разработчиком № 2'
-   return S.title()    'Function'
+def Capitalize(S):    """function создана разработчиком № 2 !!!!"""
+   return S.title()    """Function"""
 S = input()
 print (Capitalize(S))
